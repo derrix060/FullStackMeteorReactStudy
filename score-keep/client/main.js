13706bb06205b224e92d6b7168af7d3c02a86929
Mario Apra <mario_surfing@hotmail.com>
2017-10-04 16:04:30 +1300

    7. DB Planning and Rendering Static Data

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -2,6 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Meteor} from 'meteor/meteor'
 
+
+const players = [{
+  _id: '1',
+  name: 'Lauren',
+  score: 99
+},{
+  _id: '2',
+  name: 'Cory',
+  score: -1
+}, {
+  _id: '3',
+  name: 'Andrew',
+  score: -12
+}];
+
+const renderPlayers = function (playersList){
+ 
+  return playersList.map(function(player){
+    return (
+      <div key={player._id}>
+        <p>{player.name} has {player.score} point(s)</p>
+      </div>
+    )
+  })
+}
+
 Meteor.startup(function (){
   let title = 'Score Keep'
   let name = 'Mario A.'
@@ -10,6 +36,8 @@ Meteor.startup(function (){
       <h1>{title}</h1>
       <p>Hello {name}!</p>
       <p>This is my second p.</p>
+
+      {renderPlayers(players)}
     </div>
     )
 
