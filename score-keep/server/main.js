05a6bd17fc7726cc5d5d191075241e601cdb0208
Mario Apra <mario_surfing@hotmail.com>
2017-10-04 18:44:15 +1300

    ES6 Aside - Arrow Functions

--- score-keep/server/main.js
+++ score-keep/server/main.js
@@ -3,5 +3,12 @@ import {Players} from './../imports/api/players'
 
 Meteor.startup(function (){
 
-    
+    // let square = (x) => x * x
+    // console.log(square(5))
+
+    let numbers = [9, 99, 4, 56]
+    let newNumbers = numbers.map(n => n+1)
+
+    console.log(newNumbers)
+
 })
