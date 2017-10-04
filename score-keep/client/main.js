bcbf5969d14a71e849c8637dc91e93435d62f1e3
Mario Apra <mario_surfing@hotmail.com>
2017-10-04 15:01:44 +1300

    3. ES6 Aside - Importing and exporting

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -1,1 +1,8 @@
-console.log('Log from /client/main.js');
+
+import {greetUser, name} from './../imports/utils'
+
+
+console.log('Log from /client/main.js')
+console.log(greetUser())
+
+console.log(name)
