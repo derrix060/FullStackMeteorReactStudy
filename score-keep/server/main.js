8fb4276dbf54e60d8480f2e6db7e68c7a7f87bc2
Mario Apra <mario_surfing@hotmail.com>
2017-10-09 14:36:17 +1300

    ES6 Aside - Classes Part II

--- score-keep/server/main.js
+++ score-keep/server/main.js
@@ -17,9 +17,44 @@ Meteor.startup(() => {
         }
     }
 
-    let me = new Person('Mario', 22)
+    class Employee extends Person{
+        constructor (name, age, title){
+            super(name, age)
+            this.title = title
+        }
+
+        getGreeting (){
+            if (this.title) {
+                return `${super.getGreeting()} I work as a ${this.title}.`
+            }
+            return super.getGreeting()
+        }
+
+        hasJob (){
+            return !!this.title
+        }
+    }
+
+    class Programmer extends Person{
+        constructor (name, age, preferLanguage = 'assembly'){
+            super(name, age)
+            this.preferLanguage = preferLanguage
+        }
+
+        getGreeting (){
+            return `${super.getGreeting()} I'm a ${this.preferLanguage} developer.`
+            
+        }
+    }
+
+    let me = new Programmer('Mario', 22, 'db admin')
+    let mike = new Programmer('Mike', 11)
 
     console.log(me)
     console.log(me.getGreeting())
     console.log(me.getPersonDescription())
+
+    console.log(mike)
+    console.log(mike.getGreeting())
+    console.log(mike.getPersonDescription())
 })
