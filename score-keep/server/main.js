beafc27d06c329217b7bc1d5982c07d6969c9556
Mario Apra <mario_surfing@hotmail.com>
2017-10-09 14:21:45 +1300

    ES6 Aside - Classes Part I

--- score-keep/server/main.js
+++ score-keep/server/main.js
@@ -2,5 +2,24 @@ import {Meteor} from 'meteor/meteor'
 import {Players} from './../imports/api/players'
 
 Meteor.startup(() => {
+    class Person {
+        constructor (name = 'Default Name', age = 0){
+            this.name = name
+            this.age = age
+        }
 
+        getGreeting (){
+            return `Hi! I'm ${this.name}.`
+        }
+
+        getPersonDescription (){
+            return `${this.name} is ${this.age} year(s) old.`
+        }
+    }
+
+    let me = new Person('Mario', 22)
+
+    console.log(me)
+    console.log(me.getGreeting())
+    console.log(me.getPersonDescription())
 })
